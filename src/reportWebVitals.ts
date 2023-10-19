2760ea8dc531b57a57c9fb31884e607b4959351c
xodud1202 <xodud1202@gmail.com>
2023-10-19 12:07:22 +0900

    typescript 세팅

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,4 +1,5 @@
-import {ReportHandler} from "web-vitals";
+import { ReportHandler } from 'web-vitals';
+
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -9,19 +10,6 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
       getTTFB(onPerfEntry);
     });
   }
-}
-
-// const reportWebVitals = onPerfEntry => {
-//   if (onPerfEntry && onPerfEntry instanceof Function) {
-//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-//       getCLS(onPerfEntry);
-//       getFID(onPerfEntry);
-//       getFCP(onPerfEntry);
-//       getLCP(onPerfEntry);
-//       getTTFB(onPerfEntry);
-//     });
-//   }
-// };
+};
 
 export default reportWebVitals;
-
