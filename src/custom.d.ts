88765c76c7f9e252ed48aad2af6886eb7e60564b
xodud1202 <xodud1202@gmail.com>
2023-10-19 11:48:43 +0900

    typescript 세팅

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+// svg 파일을 타입스크립트에서 불러올 수 있게 설정
+declare module '*.svg' {
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string}>
+
+    const src: string;
+    export default src;
+}
