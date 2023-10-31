5471a017732a4f93d5a81a1ed3b71ca6a4a610d7
xodud1202 <xodud1202@gmail.com>
2023-10-31 15:25:53 +0900

    fetch 테스트 및 innerHtml 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import styled from '@emotion/styled';
+import {Header} from "components/Header";
+import {BlogPost} from "components/BlogPost";
+import {useEffect, useState} from "react";
+import {Button} from "./components/Button";
+import {Form} from "./components/Form";
 
 const Container = styled.div`
 	height: 100vh;
@@ -9,25 +14,40 @@ const Container = styled.div`
 	overflow: scroll;
 `;
 
-const Header = styled.div`
-	background-color: white;
-	padding: 20px;
-	width: calc(100% - 40px);
-	margin-bottom: 20px;
+const ButtonContainer = styled.div`
+	position: absolute;
+	right: 40px;
+	bottom: 40px;
 `;
 
-const Title = styled.div`
-	font-size: 1.2rem;
-	font-weight: bold;
-	text-align: center;
-`;
+interface Post {
+	readonly boardNo: number;
+	readonly title: string;
+	readonly content: string;
+}
 
 function App() {
+	const [posts, setPosts] = useState<ReadonlyArray<Post>>([]);
+	const [showForm, setShowForm] = useState(false);
+
+	useEffect(() => {
+		// fetch("https://jsonplaceholder.typicode.com/posts")
+		fetch("https://kotlin.xodud1202.kro.kr/board/boards")
+			.then(response => response.json())
+			.then(result => setPosts(result))
+			.catch(error => alert(error));
+	}, []);
+
 	return (
 		<Container>
-			<Header>
-				<Title>블로그 포스트</Title>
-			</Header>
+			<Header />
+			{posts.map(post => (
+					<BlogPost key={post.boardNo} title={post.title} body={post.content} />
+			))}
+			<ButtonContainer>
+				<Button label={"등록"} color={"gray"} onClick={() => setShowForm(true)} />
+			</ButtonContainer>
+			{showForm && <Form onClose={() => setShowForm(false)}/>}
 		</Container>
 	);
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+	background-color: white;
+	padding: 20px;
+	width: calc(100% - 40px);
+	margin-bottom: 20px;
+`;
+
+const Title = styled.div`
+	font-size: 1.2rem;
+	font-weight: bold;
+	text-align: center;
+`;
+
+export const Header = () => {
+	return (
+		<Container>
+			<Title>블로그 포스트</Title>
+		</Container>
+	)
+}
